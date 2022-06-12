eb7c62c75e70d8006adfb7bd4f30755e36cb277f
Robert Mwangi <gichukirob89@gmail.com>
2022-06-12 14:24:39 -0400

    Added comments to the js file explaining what the function is doing

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+//create an array that contains rock paper scissors
 let selection = ['Rock', 'Paper', 'Scissors'];
 
+//function that allows the computer to select elements from the array
 function computerPlay() {
     
     computerSelection = selection[Math.floor(Math.random() * selection.length)];
